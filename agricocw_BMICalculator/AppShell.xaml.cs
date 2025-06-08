769b0a0222a6f0008e615482b9d96f35716f2d0a
cwagr <agricocw@mail.uc.edu>
2025-06-08 14:14:06 -0400

    Add project files.

---
+++ agricocw_BMICalculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace agricocw_BMICalculator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
